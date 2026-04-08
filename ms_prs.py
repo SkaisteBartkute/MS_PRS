1c0e128ffdba95f5dc730a60fdd1a51c378d1fc4
Skaistė Bartkutė <skaiste.bartkute@mif.stud.vu.lt>
2026-04-08 19:28:50 +0300

    Testing the ability to open a vcf file.

--- ms_prs.py
+++ ms_prs.py
@@ -1,1 +1,13 @@
+import sys
+
 # PRS calculation for multiple sclerosis.
+
+# Parsing the base data (GWAS summary statistics) vcf file.
+
+def parse_base_data():
+    base = open(sys.argv[1],"r")
+    lines = base.readlines()
+    print(lines[0])
+    base.close()
+
+parse_base_data()
