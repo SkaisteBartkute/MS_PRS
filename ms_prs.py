f3578d9d72129886e38d4fb25b79372800df15f7
Skaistė Bartkutė <skaiste.bartkute@mif.stud.vu.lt>
2026-04-08 20:21:57 +0300

    Testing splitting the vcf file lines.

--- ms_prs.py
+++ ms_prs.py
@@ -5,9 +5,16 @@ import sys
 # Parsing the base data (GWAS summary statistics) vcf file.
 
 def parse_base_data():
+    # First argument of the file is a base data vcf file.
     base = open(sys.argv[1],"r")
     lines = base.readlines()
-    print(lines[0])
+    tmp_list = []
+    for line in lines:
+        # Ignoring metadata.
+        if not line.startswith("#"):
+            tmp_list.extend(line.split())
     base.close()
+    return tmp_list
 
-parse_base_data()
+results = parse_base_data()
+print(results)
