c775be33684c0c443a0a19455000093d148fa97b
Skaistė Bartkutė <skaiste.bartkute@mif.stud.vu.lt>
2026-04-10 13:25:58 +0300

    Creating lists to store individual SNPs.

--- ms_prs.py
+++ ms_prs.py
@@ -5,16 +5,18 @@ import sys
 # Parsing the base data (GWAS summary statistics) vcf file.
 
 def parse_base_data():
-    # First argument of the file is a base data vcf file.
+    # First argument of the program is a base data vcf file.
     base = open(sys.argv[1],"r")
     lines = base.readlines()
     tmp_list = []
+    base_SNPs = []
     for line in lines:
         # Ignoring metadata.
         if not line.startswith("#"):
-            tmp_list.extend(line.split())
+            tmp_list = line.split()
+            base_SNPs.append(tmp_list)
     base.close()
-    return tmp_list
+    return base_SNPs
 
 results = parse_base_data()
 print(results)
